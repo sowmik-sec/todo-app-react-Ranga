b1bfab20a1b4bde63495f2facc0f7c12935149f6
sowmik-sec <sowmik.sec@gmail.com>
2023-12-01 19:59:50 +0600

    explore react developer tools

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { PropTypes } from "prop-types";
 import "./Counter.css";
+import CounterButton from "./CounterButton";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
@@ -32,49 +32,4 @@ export const Counter = () => {
       />
     </div>
   );
-};
-
-const CounterButton = ({ by, incrementMethod, decrementMethod }) => {
-  const [count, setCount] = useState(0);
-  const incrementCounterFunction = () => {
-    console.log("increment clicked");
-    setCount(count + parseInt(by, 10));
-    console.log(count);
-    incrementMethod();
-  };
-  const decrementCounterFunction = () => {
-    console.log("increment clicked");
-    setCount(count - by);
-    console.log(count);
-    decrementMethod();
-  };
-
-  console.log("by = ", parseInt(by, 10));
-
-  return (
-    <div className="Counter">
-      <div>
-        <button
-          className="counterButton"
-          onClick={() => incrementCounterFunction()}
-        >
-          +{by}
-        </button>
-        <button
-          className="counterButton"
-          onClick={() => decrementCounterFunction()}
-        >
-          -{by}
-        </button>
-      </div>
-    </div>
-  );
-};
-
-CounterButton.prototype = {
-  by: PropTypes.number,
-};
-
-CounterButton.defaultProps = {
-  by: 1,
 };
