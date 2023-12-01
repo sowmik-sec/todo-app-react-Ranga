12d195dc36a5a6052783655c35e75b410f0da13b
sowmik-sec <sowmik.sec@gmail.com>
2023-12-01 17:55:26 +0600

    create decrement button

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -7,6 +7,11 @@ export const Counter = () => {
     setCount(count + 1);
     console.log(count);
   };
+  const decrementCounterFunction = () => {
+    console.log("increment clicked");
+    setCount(count - 1);
+    console.log(count);
+  };
   const [count, setCount] = useState(0);
   return (
     <div className="Counter">
@@ -17,6 +22,12 @@ export const Counter = () => {
           onClick={() => incrementCounterFunction()}
         >
           +1
+        </button>
+        <button
+          className="counterButton"
+          onClick={() => decrementCounterFunction()}
+        >
+          -1
         </button>
       </div>
     </div>
