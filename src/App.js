ffc689c8fe1e6625cad796af25b5716364f039b1
sowmik-sec <sowmik.sec@gmail.com>
2023-12-01 11:50:17 +0600

    create react component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import { Component } from "react";
 
 function App() {
-  return <div className="App">My todo application</div>;
+  return (
+    <div className="App">
+      <MyFirstComponent />
+      <MySecondComponent />
+      <MyThirdComponent />
+      <MyFourthComponent />
+    </div>
+  );
+}
+
+function MyFirstComponent() {
+  return <div className="FirstComponent">First Component</div>;
+}
+
+function MySecondComponent() {
+  return <div className="SecondComponent">Second Component</div>;
+}
+
+class MyThirdComponent extends Component {
+  render() {
+    return <div className="ThirdComponent">Third Component</div>;
+  }
+}
+
+class MyFourthComponent extends Component {
+  render() {
+    return <div className="FourthComponent">Fourth Component</div>;
+  }
 }
 
 export default App;
