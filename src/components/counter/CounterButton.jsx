dc3592ceb30fa04bbcfd2ee2cbb86ee46c3ecdfb
sowmik-sec <sowmik.sec@gmail.com>
2023-12-01 20:24:02 +0600

    refactoring react counter component

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -1,36 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { PropTypes } from "prop-types";
 
 const CounterButton = ({ by, incrementMethod, decrementMethod }) => {
-  const [count, setCount] = useState(0);
-  const incrementCounterFunction = () => {
-    console.log("increment clicked");
-    setCount(count + parseInt(by, 10));
-    console.log(count);
-    incrementMethod();
-  };
-  const decrementCounterFunction = () => {
-    console.log("increment clicked");
-    setCount(count - by);
-    console.log(count);
-    decrementMethod();
-  };
-
-  console.log("by = ", parseInt(by, 10));
-
   return (
     <div className="Counter">
       <div>
-        <button
-          className="counterButton"
-          onClick={() => incrementCounterFunction()}
-        >
+        <button className="counterButton" onClick={() => incrementMethod()}>
           +{by}
         </button>
-        <button
-          className="counterButton"
-          onClick={() => decrementCounterFunction()}
-        >
+        <button className="counterButton" onClick={() => decrementMethod()}>
           -{by}
         </button>
       </div>
