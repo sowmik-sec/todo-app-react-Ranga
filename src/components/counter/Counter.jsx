fbdcd61548d93ef816abc0e1125d79ccb2f6c92c
sowmik-sec <sowmik.sec@gmail.com>
2023-12-01 19:44:06 +0600

    move react state up - call parent Component Methods

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,25 +2,57 @@ import React, { useState } from "react";
 import { PropTypes } from "prop-types";
 import "./Counter.css";
 
-export const Counter = ({ by }) => {
+export const Counter = () => {
   const [count, setCount] = useState(0);
+  const incrementCounterParentFunction = (by) => {
+    setCount(count + by);
+  };
 
+  const decrementCounterParentFunction = (by) => {
+    setCount(count - by);
+  };
+
+  return (
+    <div>
+      <span className="count">{count}</span>
+      <CounterButton
+        by={1}
+        incrementMethod={() => incrementCounterParentFunction(1)}
+        decrementMethod={() => decrementCounterParentFunction(1)}
+      />
+      <CounterButton
+        by={2}
+        incrementMethod={() => incrementCounterParentFunction(2)}
+        decrementMethod={() => decrementCounterParentFunction(2)}
+      />
+      <CounterButton
+        by={5}
+        incrementMethod={() => incrementCounterParentFunction(5)}
+        decrementMethod={() => decrementCounterParentFunction(5)}
+      />
+    </div>
+  );
+};
+
+const CounterButton = ({ by, incrementMethod, decrementMethod }) => {
+  const [count, setCount] = useState(0);
   const incrementCounterFunction = () => {
     console.log("increment clicked");
     setCount(count + parseInt(by, 10));
     console.log(count);
+    incrementMethod();
   };
   const decrementCounterFunction = () => {
     console.log("increment clicked");
     setCount(count - by);
     console.log(count);
+    decrementMethod();
   };
 
   console.log("by = ", parseInt(by, 10));
 
   return (
     <div className="Counter">
-      <span className="count">{count}</span>
       <div>
         <button
           className="counterButton"
@@ -39,10 +71,10 @@ export const Counter = ({ by }) => {
   );
 };
 
-Counter.prototype = {
+CounterButton.prototype = {
   by: PropTypes.number,
 };
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
   by: 1,
 };
