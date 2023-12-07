e43bb5232a09047d5224f81558527ff55707d40d
sowmik-sec <sowmik.sec@gmail.com>
2023-12-07 21:37:44 +0600

    add Formik and moment Libraries to display Todo React Component

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,23 +2,60 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
+import { Field, Form, Formik } from "formik";
 
 export const TodoComponent = () => {
   const { id } = useParams();
   const { username } = useAuth();
   const [description, setDescription] = useState("");
+  const [targetDate, setTargetDate] = useState("");
   const retrieveTodo = () => {
     retrieveTodoApi(username, id)
-      .then((res) => setDescription(res.data.description))
+      .then((res) => {
+        setDescription(res.data.description);
+        setTargetDate(res.data.targetDate);
+      })
       .catch((error) => console.log(error));
   };
 
   useEffect(() => retrieveTodo, [id]);
 
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="container">
       <h1>Enter Todo Details</h1>
-      <div>description: {description}</div>
+      <div>
+        <Formik
+          initialValues={{ description, targetDate }}
+          enableReinitialize={true}
+          onSubmit={handleSubmit}
+        >
+          {(props) => (
+            <Form>
+              <fieldset className="form-group">
+                <label>Description</label>
+                <Field
+                  type="text"
+                  className="form-control"
+                  name="description"
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <label>Target Date</label>
+                <Field type="date" className="form-control" name="targetDate" />
+              </fieldset>
+              <div>
+                <button className="btn btn-success mt-5" type="submit">
+                  Save
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
